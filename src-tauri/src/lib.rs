00a2dc200f7079e7cb9e704acf5c92102ec5948d
MrGunflame <mrgunflame@protonmail.com>
2026-03-12 16:40:14 +0100

    Disable logging by default

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,31 +1,29 @@
 use serde_json::Value;
+use tauri::Manager;
 use tokio::sync::{mpsc, oneshot};
-
-use crate::prover::ResponseMessage;
 
 mod prover;
 
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
-
 #[derive(Debug)]
 pub struct State {
-    pub channel: mpsc::UnboundedSender<(String, Value, oneshot::Sender<ResponseMessage>)>,
+    pub channel: mpsc::UnboundedSender<(String, Value, oneshot::Sender<Value>)>,
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    prover::start_prover();
+    pretty_env_logger::init();
+
+    let tx = prover::start_prover();
 
     tauri::Builder::default()
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
         .invoke_handler(tauri::generate_handler![prover::send_msg])
+        .setup(|app| {
+            app.manage(State { channel: tx });
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
